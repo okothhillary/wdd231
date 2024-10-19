3ef3c9b006d0bbc138e77e211138f744c7225f8b
okothhillary <odu22002@byui.edu>
2024-10-19 19:02:14 +0300

    audit fixes

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -26,23 +26,6 @@ if (!lastVisit) {
 localStorage.setItem("lastVisit", now);
 
 
-const lazyImages = document.querySelectorAll("img.lazy");
-
-const imageObserver = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            const img = entry.target;
-            img.src = img.getAttribute("data-src");
-            img.onload = () => img.classList.add("loaded");
-            observer.unobserve(img);
-        }
-    });
-});
-
-lazyImages.forEach(img => {
-    imageObserver.observe(img);
-});
-
 const menuBtn = document.querySelector('#menu');
 const menuNav = document.querySelector('.open-menu');
 const headerElem = document.querySelector('.header');
